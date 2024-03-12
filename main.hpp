f65790afffbe0efbf96ef0c7f2b882be8c20b283
Ernesto Flores <eflores73@students.avc.edu>
2024-03-12 15:09:42 -0700

    dark

--- main.hpp
+++ main.hpp
@@ -14,10 +14,38 @@ int printTel(char[], int);
 
 int makeDB(char people[][N][L])
 {
+    int length = 0;
+    char line[256];
+    ifstream ifs;
+    ifs.open("people-100.csv");
+    if(!ifs)
+    {
+        cerr << "Error opening file" << endl;
+    }
+    while(ifs.getline(line,256))
+    {
+        int i = 0, j = 0;
+        int field = 0;
+
+        while(line[i] != '\0')
+        {
+            if(line[i] != ',')
+                people[length][field][j++] = line[i];
+            else
+            {
+                people[length][field][j] = '\0';
+                field++;
+                j = 0;
+            }
+            i++;
+        }
+        length++;
+    }
     return length;
 }
 void printRecord(char people[][N][L], int length)
 {
+    printf("NO\tID\t\tName\t\t\tEmail\t\tPhone\t\t\tBirthday\t\t\t\tJobtitle\n");
     for (int i = 0; i < length; i++)
     {
         for (int j = 0; j < N; j++)
@@ -29,9 +57,20 @@ void printRecord(char people[][N][L], int length)
 }
 void printEmail(char people[][N][L], int length)
 {
+    for(int i = 0; i < length; i++)
+    {
+        cout << people[i][5] << endl;
+    }
 }
 int getNumMale(char people[][N][L], int length)
 {
+    int cnt = 0;
+    for(int i = 0; i < length; i++)
+        if(strcmp("Male", people[i][4]) == 0)
+        cnt++;
+
+
+    return cnt;
 }
 
 /*******************************
